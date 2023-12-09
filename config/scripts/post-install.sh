0c8b82099e618da5e3e44c6b652f22abe4cc7031
Heus-Sueh <98482469+Heus-Sueh@users.noreply.github.com>
2023-12-09 20:44:40 -0300

    Second Commit

---
+++ config/scripts/post-install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+# set sddm as default
+#systemctl set-default graphical.target 
+
+
